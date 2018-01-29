7e59905a78b0d6922b3c3ba8661ff5c2440f9a8b
Pramit Mitra <pramit.mitra@hotmail.com>
2018-01-29 15:19:16 -0800

    Create code1_basic_forloop.py

---
+++ code1_basic_forloop.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+### Problem 1, Basic For loop in Python
+ for num in range(10,20):
+     if num%2 ==0:
+         print('%d num is Odd Numner' %(num))
+     else:
+         print("Even Number")
