772b97d9a78c6e8bab7663cd6cba7b68a5925c39
Pramit Mitra <pramit.mitra@hotmail.com>
2018-01-29 15:21:45 -0800

    Update code1_basic_forloop.py

--- code1_basic_forloop.py
+++ code1_basic_forloop.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 ### Problem 1, Basic For loop in Python
- for num in range(10,20):
+for num in range(10,20):
      if num%2 ==0:
          print('%d num is Odd Numner' %(num))
      else:
