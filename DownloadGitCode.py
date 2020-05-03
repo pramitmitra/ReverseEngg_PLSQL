ab74b744c4ef862bf1def04f0a7c942ee97ca426
prammitr <pramit.mitra@oracle.com>
2020-05-03 00:19:30 -0700

    2

--- DownloadGitCode.py
+++ DownloadGitCode.py
@@ -1,8 +1,13 @@
 # Suppoting code to download git repo.
 import os
 import subprocess
+import configparser
 
+config = configparser.ConfigParser()
+config.read('pyconfig.ini')
+GIT_REPO = config['DEFAULT']['GIT_REPO'] 
 args = ['git', 'clone', '--depth=1', 'git@github.com:pramitmitra/ReadingNotes.git']
+#args = ['git', 'clone', '--depth=1', GIT_REPO]
 res = subprocess.Popen(args, stdout=subprocess.PIPE)
 output, _error = res.communicate()
 os.system('cp /Users/prammitr/Documents/my_projects/python/ReadingNotes/input_plSql.sql /Users/prammitr/Documents/my_projects/python/input')
@@ -12,5 +17,3 @@ if not _error:
     print(output)
 else:
     print(_error)
-
-
