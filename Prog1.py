bcdea477bf55027c9488892c19d291ae658b4ea4
Pramit Mitra <pramit.mitra@oracle.com>
2020-08-10 10:49:47 -0700

    Commit 2

--- Prog1.py
+++ Prog1.py
@@ -1,19 +1,23 @@
 # print ('Hello, world!')
 
+#2 lines: Input, assignment
 # name = input('What is your name?\n')
 # print ('Hi, %s.' % name)
 
+#3 lines: For loop, built-in enumerate function, new style formatting
 # friends = ['john', 'pat', 'gary', 'michael']
 # for i, name in enumerate(friends):
 #     print ("iteration {iteration} is {name}".format(iteration=i, name=name))
 
 
+#4 lines: Fibonacci, tuple assignment
 # parents, babies = (1, 1)
 # while babies < 100:
 #     print ('This generation has {0} babies'.format(babies))
 #     parents, babies = (babies, parents + babies)
 
 
+#5 lines: Functions
 # def greet(name):
 #     print ('Hello', name)
 
@@ -22,10 +26,20 @@
 # greet('Bob')
 
 
+#6 lines: Import, regular expressions
+# Regular Expression to Validat Stuff
+# import re
+# for test_string in ['555-1212', 'ILL-EGAL']:
+#     if re.match(r'^\d{3}-\d{4}$', test_string):
+#         print (test_string, 'is a valid US local phone number')
+#     else:
+#         print (test_string, 'rejected')
 
-import re
-for test_string in ['555-1212', 'ILL-EGAL']:
-    if re.match(r'^\d{3}-\d{4}$', test_string):
-        print (test_string, 'is a valid US local phone number')
-    else:
-        print (test_string, 'rejected')
+
+#8  lines: Command line arguments, exception handling
+import sys
+try:
+    total = sum(int(arg) for arg in sys.argv[1:])
+    print ('sum =', total)
+except ValueError:
+    print ('Please supply integer arguments')
